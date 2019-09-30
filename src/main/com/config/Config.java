6f463e5a1cde2b9d26a8b9de556a36a50a5fe7c7
Mehul Prajapati <mehulprajapati2802@gmail.com>
2019-09-29 23:19:12 -0400

    Add player details

--- src/main/com/config/Config.java
+++ src/main/com/config/Config.java
@@ -1,5 +1,48 @@
 package com.config;
 
+/**
+ * This class defines static properties 
+ * for the game such as number of armies 
+ * according to the number of players.
+ * @author Mehul
+ */
 public class Config {
 
+	/**
+	 * The message to be printed in text after attack phase is completed 
+	 */
+	public static String message = "";
+
+	/**
+	 * The CONFIG_ARMIES_TWO_PLAYER Constant assigns 40 armies to 2 player in the game 
+	 */
+	public static final Integer CONFIG_ARMIES_TWO_PLAYER = 40;
+	
+	/**
+	 * The CONFIG_ARMIES_THREE_PLAYER Constant assigns 35 armies to 3 player in the game 
+	 */
+	public static final Integer CONFIG_ARMIES_THREE_PLAYER = 35;
+	
+	/**
+	 * The CONFIG_ARMIES_FOUR_PLAYER Constant assigns 30 armies to 4 player in the game 
+	 */
+	public static final Integer CONFIG_ARMIES_FOUR_PLAYER = 30;
+	
+	/**
+	 * The CONFIG_ARMIES_FIVE_PLAYER Constant assigns 25 armies to 5 player in the game 
+	 */
+	public static final Integer CONFIG_ARMIES_FIVE_PLAYER = 25;
+	
+	/**
+	 * The CONFIG_ARMIES_SIX_PLAYER Constant assigns 20 armies to 6 player in the game 
+	 */
+	public static final Integer CONFIG_ARMIES_SIX_PLAYER = 20;
+	
+	public static boolean isGameOver = false;
+	public static boolean isAllComputerPlayer = true;
+	public static int waitBeweenTurn = 3000; // 7 sec
+	
+	public static boolean isPopUpShownInAutoMode = true;
+	public static boolean isTournamentMode = false;
+	public static boolean isThreadingForTournament = false;
 }
