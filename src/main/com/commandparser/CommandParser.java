90fd764a28876447741927ded1eb8406e331fbcc
Parth Patel <patelparth_28@yahoo.com>
2019-10-05 14:41:25 -0400

    Parsing editcountry and added javadoc

--- src/main/com/commandparser/CommandParser.java
+++ src/main/com/commandparser/CommandParser.java
@@ -1,7 +1,10 @@
 package com.commandparser;
 
 public class CommandParser {
-
+    /**
+     * Parses the String and calls the relative function.
+     * @param command User input Command/String to be parse
+     */
     public CommandParser(String command){
 
         String[] words=command.split(" ");
@@ -25,9 +28,35 @@ public class CommandParser {
                     String continentname=words[2];
                     System.out.println("remove:"+words[2]);
                     //Call for removing the continent name with (continentname) as parameters
+
+                }else{
+
+                    System.out.println("Check input!!");
+
                 }
                 break;
 
+            case "editcountry":
+                if(words[1].equals("-add")){
+
+                    String continentname=words[2];
+                    String countryname=words[3];
+                    System.out.println("Editcountry -add:"+continentname+" "+countryname);
+
+                    //Call for adding the country name with (continentname,countryname) as parameters
+
+                }else if(words[1].equals("-remove")){
+
+                    String countryname=words[2];
+                    System.out.println("Editcountry -remove:"+countryname);
+
+                    //Call for removing the country name with (countryname) as parameter
+
+                }else{
+
+                    System.out.println("Wrong input!!");
+                }
+                break;
             default:
                 System.out.println("Check the input!!");
                 break;
