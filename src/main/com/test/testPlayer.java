0c40c40da1e004454263555ad232023869ad1c0b
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2019-10-11 16:04:31 -0400

    Add BeforeClass and Before

--- src/main/com/test/testPlayer.java
+++ src/main/com/test/testPlayer.java
@@ -1,23 +1,37 @@
 package com.test;
 import static org.junit.Assert.*;
+import org.junit.*;
 
-import org.junit.Test;
 
 import com.entity.Country;
 import com.entity.Player;
 
 public class testPlayer {
 	/**
-	 * Test equal method in player class.
+	 * Test all methods in Player class.
 	 * @author  Maryam
+	 * @author Mahmoudreza
 	 */
+	Player ply = null;
+	@BeforeClass
+	public void beforeAll() {
+		System.out.println("This is test for ...");
+	}
+	@Before
+	//we need to edit
+	public void beforePlayer() {
+		ply = new Player(1,"Test");
+	}
 	@Test
 	public void testEqual() {
-		Player p = new Player(1, "sara");
-
-		boolean value = p.equals(p);
+		boolean value = ply.equals(ply);
 		boolean correctValue = true;
 		assertEquals(correctValue, value);
+	}
+	@Test
+	//--> ...
+	public void test(){
+
 	}
 
 }
