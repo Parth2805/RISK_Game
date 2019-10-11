446a016946207d26561efc2b6c9f5b0ddb731de2
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2019-10-11 16:29:25 -0400

    Performed some few edits

--- src/main/com/test/TestPlayer.java
+++ src/main/com/test/TestPlayer.java
@@ -1,37 +1,44 @@
 package com.test;
 import static org.junit.Assert.*;
 import org.junit.*;
-
-
 import com.entity.Country;
 import com.entity.Player;
 
-public class testPlayer {
-	/**
-	 * Test all methods in Player class.
-	 * @author  Maryam
-	 * @author Mahmoudreza
-	 */
+/**
+ * Test all methods in Player class.
+ * @author  Maryam
+ * @author Mahmoudreza
+ */
+
+public class TestPlayer {
+
 	Player ply = null;
+
 	@BeforeClass
 	public void beforeAll() {
 		System.out.println("This is test for ...");
 	}
+
 	@Before
-	//we need to edit
 	public void beforePlayer() {
 		ply = new Player(1,"Test");
 	}
+
+	@After
+	public void afterPlayer(){
+		System.out.println("After each method");
+	}
+
 	@Test
 	public void testEqual() {
 		boolean value = ply.equals(ply);
 		boolean correctValue = true;
 		assertEquals(correctValue, value);
 	}
-	@Test
-	//--> ...
-	public void test(){
 
+	@AfterClass
+	public void afterAll() {
+		System.out.println("All tests performed");
 	}
 
 }
