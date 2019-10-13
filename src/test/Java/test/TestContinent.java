fbba4b916f3cf4ae3017e6e1435bc043c41b30f5
Mahmoudreza Entezami <mahmoudreza.entezami@gmail.com>
2019-10-13 17:17:49 -0400

    Add Uni_test classes and files

---
+++ src/test/Java/test/TestContinent.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+import com.entity.Continent;
+
+
+public class TestContinent {
+    Continent cont = null;
+
+    @BeforeClass
+    public static void beforeAllTesting() {
+        System.out.println("This is before testing");
+    }
+    @Before
+    public void beforeTest() {
+        cont = new Continent();
+    }
+
+    @AfterClass
+    public static void afterPerformingTests() {
+        System.out.println("The test is done");
+    }
+
+    @Test
+    public void testGetColor() {
+        assertNull(cont.getColor());
+        System.out.println("'assertNull' test for getColor method is passed");
+        assertNotEquals("Red", cont.getColor());
+        System.out.println("'assertNotEqual' test for getColor method is passed");
+    }
+
+    @Test
+    public void testGetName() {
+        assertNull(cont.getName());
+        System.out.println("'assertNull' test for getName method is passed");
+        assertNotEquals("Advance Programming", cont.getName());
+        System.out.println("'assertNotEqual' test for getName method is passed");
+    }
+
+}
