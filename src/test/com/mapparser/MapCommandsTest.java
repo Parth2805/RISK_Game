f01e13703ac3cd0e431d9341302674671d4304c5
maryamgiahi <maryamgiahi59@gmail.com>
2019-10-15 02:17:03 +0330

    update the MapcommandTest

--- src/test/com/mapparser/MapCommandsTest.java
+++ src/test/com/mapparser/MapCommandsTest.java
@@ -18,7 +18,12 @@ import com.entity.Hmap;
 import com.exception.InvalidMap;
 import com.mapparser.MapCommands;
 
-
+/**
+ * This is a Test Class for testing MapCommand
+ * @author Maryam
+ * @version 0.0.1
+ *
+ */
 public class MapCommandsTest {
   MapCommands mapcom;
   static Continent continent;
@@ -44,15 +49,23 @@ public class MapCommandsTest {
   String mapScroll = "horizontal";
   String mapWarn = "yes";
   
+  /**
+   	* This method runs before All Testing an run it just one time
+	*/
+  
   @BeforeClass
     public static void beforeAllTesting() {
-    System.out.println("This is for testing Player Class");
+    System.out.println("This is for testing MapCommand Class");
     map = new Hmap();
     mapData = new HashMap<>();
     continent = new Continent();
     country = new Country();
     adjCountry =  new Country();
     }
+  
+  /**
+ 	* This method runs before All test case and run before each test cases  
+	*/
     @Before
     public void beforeTest() {
     mapData = new HashMap<>();
@@ -64,26 +77,37 @@ public class MapCommandsTest {
     map.setMapData(mapData);
     }
     
+    /**
+   	* This method runs After All Testing 
+	*/
     @AfterClass
     public static void afterPerformingTests() {
         System.out.println("The test is done");
     }
 
+    /**
+   	* This method Test the remove Continent  
+	*/
     @Test
     public void removeContinentTest() {
       
-       System.out.println(" This is a test for Remove Continet");
+       System.out.println("This is a test for Remove Continet");
        assertEquals(true, mapcom.removeContinent(map,nameContinet));
     }
   
-    
+    /**
+   	* This method Test the add Continent 
+	*/
    @Test 
     public void addContinentTest() throws InvalidMap {
      
-     System.out.println(" This is a test for Add Continet it was pass");
+     System.out.println("This is a test for Add Continet it was pass");
     assertEquals(true,mapcom.addContinent(map, nameContinet, String.valueOf(ctrlValue1), color));
     }
    
+   /**
+  	* This method Test the add Continent   
+	*/
    @Test 
    public void addContinentTest2() throws InvalidMap {
     
@@ -91,9 +115,12 @@ public class MapCommandsTest {
 		assertNotNull(output);
    }
     
+   /**
+  	* This method Test the update Continent  
+	*/
    @Ignore 
     public  void updateContinentTest() throws InvalidMap{
-      System.out.println(" This is a test for Update Continet"); 
+      System.out.println("This is a test for Update Continet"); 
       continent  = mapcom.updateContinent(continent, map, nameContinet, String.valueOf(ctrlValue2));
     		 // mapcom.updateContinent(continent, map, nameContinet, String.valueOf(ctrlValue2));
    //   assertEquals(continent.getValue(), ctrlValue1);
@@ -101,26 +128,35 @@ public class MapCommandsTest {
      assertEquals(continent.getName(), nameContinet);
     }
     
+   /**
+  	* This method Test the add country  
+	*/
     @Test 
-    public void addCountryTest()  throws InvalidMap{
-      System.out.println(" This is a test for UpdateCountry Continet");
+    public void addCountryTest() throws InvalidMap{
+      System.out.println("This is a test for AddCountry Continet");
       boolean output=MapCommands.addCountry(map, nameCountry, nameContinet);
 		assertNotNull(output);
     }
   
-  @Ignore
+    /**
+   	* This method Test the update country  
+	*/
+    @Ignore
     public void updateCountryTest() throws InvalidMap {
-      System.out.println(" This is a test for UpdateCountry Continet");
-      country= mapcom.updateCountry(adjCountry, map, nameCountry, String.valueOf(xCo1), String.valueOf(yCo1), adjCoun) ;
+      System.out.println("This is a test for UpdateCountry Continet");
+      country= mapcom.updateCountry(country, map, nameCountry, String.valueOf(xCo1), String.valueOf(yCo1),adjCountry ) ;
       //assertNull(mapcom.updateCountry(country, map, nameCountry, xCo1, yCo1, adjCoun));
-      assertEquals(country.getxCoordinate(), xCo1);
+     // assertEquals(country.getxCoordinate(), xCo1);
       assertNotNull(country);
       
     }
     
+    /**
+   	* This method Test map country to continent   
+	*/
    @Test 
     public void mapCountryToContinentTest() {
-      System.out.println(" This is a test for mapCountry Continet");
+      System.out.println("This is a test for mapCountry to Continet");
       assertNotNull(mapcom.mapCountryToContinent(continent, country));
     }
 
