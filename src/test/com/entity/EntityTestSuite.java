637e5a2470d84a6f71472744c7bc76aea90f2b53
maryamgiahi <maryamgiahi59@gmail.com>
2019-10-15 09:48:57 +0330

    Add Test Entity Suite

---
+++ src/test/com/entity/EntityTestSuite.java
@@ -0,0 +1,21 @@
+package com.entity;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestCard.class,
+				TestContinent.class,
+				TestCountry.class,
+				TestPlayerClass.class
+				})
+/**
+ * This is a Test suite Class for testing Entity
+ * @author Maryam
+ * @author Mahmoudreza
+ * @version 0.0.1
+ */
+public class EntityTestSuite {
+
+}
