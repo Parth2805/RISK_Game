d173794fe12081f249b525ae721d48609474df3e
maryamgiahi <maryamgiahi59@gmail.com>
2019-10-15 09:33:05 +0330

    Add MapParserSuiteTest

--- src/test/com/mapparser/MapParserTestSuite.java
+++ src/test/com/mapparser/MapParserTestSuite.java
@@ -5,7 +5,16 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({MapReaderTest.class})
+@SuiteClasses({MapReaderTest.class,
+				MapCommandsTest.class,
+				MapVerifierTest.class
+				})
+/**
+ * This is a Test suite Class for testing MapParser
+ * @author Maryam
+ * @author Mahmoudreza
+ * @version 0.0.1
+ */
 
 public class MapParserTestSuite {
 
